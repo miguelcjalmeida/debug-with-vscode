444004946292d7dafe03266f09952da8882e8b5b
Miguel Almeida <darkmagusbr@gmail.com>
2016-10-13 00:39:57 -0300

    add examples for debugging with .vscode

---
+++ javascript/scripts/script.js
@@ -0,0 +1,5 @@
+var element = document.getElementById("content");
+
+for(var i=0; i<10; i++){
+    element.innerHTML += "!";
+}
---
+++ typescript/scripts/script.ts
@@ -0,0 +1,5 @@
+let element : HTMLElement = document.getElementById("content");
+
+for(let i : number =0; i<10; i++){
+    element.innerHTML += "!";
+}
